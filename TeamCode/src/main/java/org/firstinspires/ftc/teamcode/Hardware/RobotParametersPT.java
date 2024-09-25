a5a604c28c13e6c5074a7fb50a576d5db51d7c58
FTCTe <ftcteam23493@gmail.com>
2024-09-25 19:26:33 -0400

    2 slides code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RobotParametersPT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RobotParametersPT.java
@@ -16,6 +16,7 @@ public final class RobotParametersPT{
     public static final double powerReduction = 0.75;
 
 
+
     public enum IntakeState {
         PULL_IN,
         PUSH_OUT,
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/SlidePT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/SlidePT.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Hardware.RobotParametersPT;
-@TeleOp(name="SlidesPT", group="TeleOp")
 public class SlidePT {
     private final DcMotor SlideMotor1;
     private final DcMotor SlideMotor2;
@@ -14,6 +13,9 @@ public class SlidePT {
     public SlidePT(RobotParametersPT params, HardwareMap hardwareMap){
         SlideMotor1 = hardwareMap.get(DcMotor.class, params.slideMotorName1);
         SlideMotor2 = hardwareMap.get(DcMotor.class, params.slideMotorName2);
+        SlideMotor2.setDirection(DcMotor.Direction.REVERSE);
+        SlideMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        SlideMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
     }
 
@@ -24,34 +26,30 @@ public class SlidePT {
                 break;
 
             case SLIDE_OUT:
-                slideOut1(power);
-                slideOut2(power);
+                slideOut(-power);
                 break;
 
             case STOP:
-                stop1();
-                stop2();
+                stop();
                 break;
         }
     }
 
     public void slideIn(double power){
         SlideMotor1.setPower(power);
+      SlideMotor2.setPower(power);
+    }
+    public void slideOut(double power){
+        SlideMotor1.setPower(power);
         SlideMotor2.setPower(power);
     }
 
-    public void slideOut1(double power){
-        SlideMotor1.setPower(-power);
-    }
-    public void slideOut2(double power){
-        SlideMotor2.setPower(-power);
+
+    public void stop(){
+        SlideMotor1.setPower(0);
+        SlideMotor2.setPower(0);
+
     }
 
-    public void stop1(){
-        SlideMotor1.setPower(0);
-    }
-    public void stop2(){
-        SlideMotor1.setPower(0);
-    }
 
 }
