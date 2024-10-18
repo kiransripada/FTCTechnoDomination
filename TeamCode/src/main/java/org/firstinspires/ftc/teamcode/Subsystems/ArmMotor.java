40be67c5d869e1112cc4afaeca7219b44d559bd3
FTCTe <leilanie.premkumar12@gmail.com>
2024-10-18 13:48:41 -0400

    Added arm motor code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ArmMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ArmMotor.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.Hardware.RobotParametersPT;
+import com.qualcomm.robotcore.hardware.IMU;
 
 public class ArmMotor {
     private RobotParametersPT params;
@@ -12,6 +16,10 @@ public class ArmMotor {
 
     public ArmMotor(RobotParametersPT params, HardwareMap hardwareMap){
         ArmMotor = hardwareMap.get(DcMotor.class, params.armMotorName);
+
+        ArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
     }
 
     public void stateUpdate(RobotParametersPT.ArmState armState, double power){
@@ -38,8 +46,7 @@ public class ArmMotor {
         ArmMotor.setPower(-power);
     }
 
-    public void stop(){
+    public void stop() {
         ArmMotor.setPower(0);
     }
-
 }
